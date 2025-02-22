299114638d48f062bf9f035faebb4d5a4ba3c528
szymonrybczak <szymon.rybczak@gmail.com>
2025-02-22 14:56:45 +0100

    feat: use mistral

--- lib/ai/models.ts
+++ lib/ai/models.ts
@@ -1,23 +1,13 @@
 import { openai } from '@ai-sdk/openai';
-import { fireworks } from '@ai-sdk/fireworks';
 import {
   customProvider,
-  extractReasoningMiddleware,
-  wrapLanguageModel,
 } from 'ai';
-
+import { mistral } from '@ai-sdk/mistral';
 export const DEFAULT_CHAT_MODEL: string = 'chat-model-small';
 
 export const myProvider = customProvider({
   languageModels: {
-    'chat-model-small': openai('gpt-4o-mini'),
-    'chat-model-large': openai('gpt-4o'),
-    'chat-model-reasoning': wrapLanguageModel({
-      model: fireworks('accounts/fireworks/models/deepseek-r1'),
-      middleware: extractReasoningMiddleware({ tagName: 'think' }),
-    }),
-    'title-model': openai('gpt-4-turbo'),
-    'artifact-model': openai('gpt-4o-mini'),
+    default: mistral('mistralai/Mistral-7B-v0.1'),
   },
   imageModels: {
     'small-model': openai.image('dall-e-2'),
@@ -33,18 +23,8 @@ interface ChatModel {
 
 export const chatModels: Array<ChatModel> = [
   {
-    id: 'chat-model-small',
-    name: 'Small model',
-    description: 'Small model for fast, lightweight tasks',
-  },
-  {
-    id: 'chat-model-large',
-    name: 'Large model',
-    description: 'Large model for complex, multi-step tasks',
-  },
-  {
-    id: 'chat-model-reasoning',
-    name: 'Reasoning model',
-    description: 'Uses advanced reasoning',
-  },
+    id: 'default',
+    name: 'Default model',
+    description: 'Default model for fast, ',
+  }
 ];
